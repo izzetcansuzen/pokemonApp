1e1a31a998da6a6ca724f928295d66133adb53fe
izzetcansuzen <izzetcansuzen@gmail.com>
2024-04-23 15:03:04 +0300

    completed get card detail fetch

--- app/detail/[id]/page.tsx
+++ app/detail/[id]/page.tsx
@@ -1,10 +1,40 @@
+"use client"
+import {useEffect, useState} from "react";
+import {fetchCard, fetchCards} from "@/utils/api";
+interface Card {
+    id: string;
+    name: string;
+    images: {
+        small: string
+    };
+}
 export default function DetailID(){
     //TODO: Bu sayfa tasarımsal olarak düzenlenmeli
     //TODO: Sayfada istek atılıp istenen pokemona göz atılmalı
     //TODO: Gelecek değerler => Card type, HP, abilities
     //TODO: Kullanıcı isterse bunu save yapıp localstorage'e kaydeder
     //TODO: Kullanıcı daha önce kaydetmişse remove butonu olup isterse remove edebilir
+
+    const [card, setCard] = useState<Card | null >(null)
+
+    useEffect(() => {
+        const fetchCardData = async () => {
+            const cardData = await fetchCard()
+            setCard(cardData);
+        };
+
+        fetchCardData();
+    }, []);
+
+    useEffect(() => {
+        console.log(card)
+    }, [card]);
+
     return (
-        <div>Detail ID Page</div>
+        <>
+            {/*Image Container*/}
+            <div>
+            </div>
+        </>
     )
 }
--- utils/api.ts
+++ utils/api.ts
@@ -3,3 +3,10 @@ export const fetchCards = async () => {
     const data = await res.json();
     return data.data;
 };
+
+/*değer dinamik olarak güncellenmeli*/
+export const fetchCard = async () => {
+    const res = await fetch('https://api.pokemontcg.io/v2/cards/hgss4-1');
+    const data = await res.json();
+    return data;
+};
